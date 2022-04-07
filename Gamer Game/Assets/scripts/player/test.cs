9991cdb848ca4313cbed72e48fb0c770bb6ec55d
RTXT25 <skoolhumanboi@gmail.com>
2022-04-07 21:05:15 +1000

    adwefdghngvfdg

---
+++ Gamer Game/Assets/scripts/player/test.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour{
+    void Start(){
+        Destroy(gameObject, 0.5f);
+    }
+}
