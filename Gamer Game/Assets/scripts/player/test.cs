ad038ab17b496e6a816c6be995a2947380afb62a
RTXT25 <skoolhumanboi@gmail.com>
2022-05-08 15:15:16 +1000

    aaaaaa

--- Gamer Game/Assets/scripts/player/test.cs
+++ Gamer Game/Assets/scripts/player/test.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test : MonoBehaviour{
+
+    public float rot=0f;
+    public float speed= 1f;
+    
     void Start(){
+        float rot = Camera.main.transform.localEulerAngles.y;
+        transform.rotation = Quaternion.Euler(0f, rot, 0f);
+        Vector3 moveDir = Quaternion.Euler(0f, rot, 0f) * Vector3.forward;
+        //transform.localPosition =Quaternion.Euler(1f, 1f, 1f);
         Destroy(gameObject, 0.5f);
     }
 }
--- Gamer Game/Assets/scripts/player/testspawn.cs
+++ Gamer Game/Assets/scripts/player/testspawn.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class testspawn : MonoBehaviour
-{
+{   
+    public GameObject player;
     public GameObject myPrefab;
+    Vector3 place;
+    public float rot=0f;
     public float px;
     public float py;
     public float pz;
@@ -14,8 +17,11 @@ public class testspawn : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+        float rot = Camera.main.transform.localEulerAngles.y;
+        transform.rotation = Quaternion.Euler(0f, rot, 0f);
+        Vector3 place = gameObject.transform.position;
         if (Input.GetKeyDown(KeyCode.Mouse0)){
-        Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(myPrefab, place, Quaternion.identity);
         }
     }
 }
